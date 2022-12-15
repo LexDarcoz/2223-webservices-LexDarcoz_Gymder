930aff74670a7dab840a4c72d555ebca8a1dc539
LexDarcoz <Alexander.schatteman@gmail.com>
2022-12-15 22:03:50 +0100

    hmmmmmmmmmm

--- config/production.js
+++ config/production.js
@@ -5,13 +5,13 @@ module.exports = {
     disabled: false,
   },
   cors: {
-    origins: ["https://frontendweb-budget.onrender.com"],
+    origins: ["https://gymder.onrender.com/"],
     maxAge: 3 * 60 * 60, // 3h in seconds
   },
   database: {
-    client: "mysql2",
-    host: "localhost",
-    port: 3306,
-    name: "budget",
+    client: "182201as",
+    host: "vichogent.be",
+    port: 40043,
+    name: "182201as",
   },
 };
